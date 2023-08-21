53c7cd1553c54758b703dedad9cfcdc772b0b7c0
Brendan Moran <b.daniel.moran@gmail.com>
2023-08-21 16:57:08 -0400

    try this again

--- install_util.sh
+++ install_util.sh
@@ -1,4 +1,4 @@
 #copy mini_util.sh to /usr/bin
 sudo cp mini_util.sh /usr/bin/mini_util.sh
 sudo chown root:users /usr/bin/mini_util.sh
-sudo chmod 4550 /usr/bin/mini_util.sh
+sudo chmod 750 /usr/bin/mini_util.sh
