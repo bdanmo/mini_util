8df906bcb5630d9e214fc057d703d1c9b8e4836b
Brendan Moran <b.daniel.moran@gmail.com>
2023-08-21 16:08:18 -0400

    init commit

---
+++ mini_util.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+function show_ipinfo() {
+#create var, ip_var, to show the ip address of the active interface. i.e., inet field is present -- exclude loopback (lo)
+ip_var=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}')
+#create var, dgw_var, to show default gateway of this same interface
+dgw_var=$(ip route show | grep 'default' | awk '{print $3}')
+echo "IP Address: $ip_var"
+echo "Default Gateway: $dgw_var"
+cat /etc/resolv.conf | grep "nameserver" | grep -v "#"
+}
+
+while true
+do
+clear
+echo
+echo "UTILITY MENU"
+echo "------------"
+echo
+echo "1 - Show IP Information"
+echo
+echo "2 - Show currently logged in user"
+echo
+echo "3 - Quit"
+echo
+echo -n "Enter your choice: "
+echo
+read choice
+case $choice in
+1) show_ipinfo ;;
+2) whoami ;;
+3) clear;exit 0 ;;
+*) echo "Invalid choice" ;;
+esac
+echo -n "Press Enter to cmake another selection"
+read junk
+done
+
+
