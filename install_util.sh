78e8f24fe7fb10c0242bf8a4582448ebf1d2790b
Brendan Moran <b.daniel.moran@gmail.com>
2023-08-22 17:42:50 -0400

    make executable for all users in /bin/usr

--- install_util.sh
+++ install_util.sh
@@ -1,4 +1,4 @@
 #copy mini_util.sh to /usr/bin
 sudo cp mini_util.sh /usr/bin/mini_util.sh
 sudo chown root:users /usr/bin/mini_util.sh
-sudo chmod 750 /usr/bin/mini_util.sh
+sudo chmod 755 /usr/bin/mini_util.sh
